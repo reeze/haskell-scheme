cb1f521fb1ba175611bba39fc9660cf225f10c72
Reeze Xia <reeze@php.net>
2015-01-14 01:29:15 +0800

    Implemented function and closure

--- parser.hs
+++ parser.hs
@@ -37,8 +37,8 @@ setVar envRef var value = do env <- liftIO $ readIORef envRef
                                    (lookup var env)
                              return value
 
-defineVal :: Env -> String -> LispVal -> IOThrowsError LispVal
-defineVal envRef var value = do
+defineVar :: Env -> String -> LispVal -> IOThrowsError LispVal
+defineVar envRef var value = do
 	alreadDefined <- liftIO $ isBound envRef var
 	if alreadDefined
 		then setVar envRef var value >> return value
@@ -54,6 +54,9 @@ bindVars envRef bindings = readIORef envRef >>= extendEnv bindings >>= newIORef 
     addBinding (var, value) = do ref <- newIORef value
                                  return (var, ref)
 
+makeFunc varargs env params body = return $ Func (map showVal params) varargs body env
+makeNormalFunc = makeFunc Nothing
+makeVarArgs = makeFunc . Just . showVal
 
 -- REPL
 flushStr :: String -> IO ()
@@ -71,10 +74,10 @@ evalAndPrint env "" = putStrLn ""
 evalAndPrint env expr = evalString env expr >>= putStrLn
 
 runOne :: String -> IO ()
-runOne expr = nullEnv >>= flip evalAndPrint expr
+runOne expr = primitiveBindings >>= flip evalAndPrint expr
 
 runRepl :: IO ()
-runRepl = nullEnv >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
+runRepl = primitiveBindings >>= until_ (== "quit") (readPrompt "Lisp>>> ") . evalAndPrint
 
 until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
 until_ pred prompt action = do
@@ -94,6 +97,9 @@ data LispVal = Atom String
 			 | String String
 			 | Nil
 			 | Bool Bool
+			 | PrimitiveFunc ([LispVal] -> ThrowsError LispVal)
+			 | Func { params :: [String], vararg :: (Maybe String),
+			          body :: [LispVal], closure :: Env }
 
 data LispError = NumArgs Integer [LispVal]
                | TypeMismatch String LispVal
@@ -203,6 +209,12 @@ showVal (Bool False) = "#f"
 showVal (List contents) = "(" ++ unwordsList contents ++ ")"
 showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
 showVal (Nil) = "Nil"
+showVal (PrimitiveFunc _) = "<primitive>"
+showVal (Func {params = args, vararg = varargs, body = body, closure = env}) = 
+	"(lambda (" ++ unwords (map show args) ++
+		(case varargs of
+			Nothing -> ""
+			Just arg -> " . " ++ arg) ++ ") ...)"
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
@@ -223,14 +235,41 @@ eval env (List [Atom "if", pred, yescond, nocond]) =
                                                        Bool True -> eval env yescond
                                                        other -> throwError $ TypeMismatch "Pred must be boolean" other
 eval env (List [Atom "set!", Atom var, form]) = eval env form >>= setVar env var
-eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVal env var
-eval env (List (Atom func : args)) = mapM (eval env) args >>= liftThrows . apply func
+eval env (List [Atom "define", Atom var, form]) = eval env form >>= defineVar env var
+eval env (List (Atom "define" : List (Atom var : params) : body)) = 
+	makeNormalFunc env params body >>= defineVar env var
+eval env (List (Atom "define" : DottedList (Atom var : params) varargs : body)) =
+     makeVarArgs varargs env params body >>= defineVar env var
+eval env (List (Atom "lambda" : List params : body)) =
+     makeNormalFunc env params body
+eval env (List (Atom "lambda" : DottedList params varargs : body)) =
+     makeVarArgs varargs env params body
+eval env (List (Atom "lambda" : varargs@(Atom _) : body)) =
+	makeVarArgs varargs env [] body
+eval env (List (function : args)) = do
+	func <- eval env function
+	argVals <- mapM (eval env) args
+	apply func argVals
 eval env badForm = throwError $ BadSpecialForm "Unrecogizaed special form" badForm
 
-apply :: String -> [LispVal] -> ThrowsError LispVal
-apply func args = maybe (throwError $ NotFunction "Unrecogizaed primitive function args" func)
-                        ($ args)
-						(lookup func primitives)
+apply :: LispVal -> [LispVal] -> IOThrowsError LispVal
+apply (PrimitiveFunc func) args = liftThrows $ func	args
+apply (Func params varargs body closure) args = 
+	if num params /= num args && varargs == Nothing
+		then throwError $ NumArgs (num params) args
+		else (liftIO $ bindVars closure $ zip params args) >>= bindVarArgs varargs >>= evalBody
+	where
+		remainingArgs = drop (length params) args
+		num = toInteger . length
+		evalBody env = liftM last $ mapM (eval env) body
+		bindVarArgs arg env = case arg of
+			Just argName -> liftIO $ bindVars env [(argName, List $ remainingArgs)]
+			Nothing -> return env
+		
+
+primitiveBindings :: IO Env
+primitiveBindings = nullEnv >>= (flip bindVars $ map makePrimitiveFunc primitives)
+	where makePrimitiveFunc (var, func) = (var, PrimitiveFunc func)
 
 primitives :: [(String, [LispVal] -> ThrowsError LispVal)]
 primitives = [("+", numericBinop (+)),
@@ -271,7 +310,6 @@ numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> ThrowsError Lisp
 numericBinop op            [] = throwError $ NumArgs 2 []
 numericBinop op singleVal@[_] = throwError $ NumArgs 2 singleVal
 -- It is strict 2 operator by reeze
--- numericBinop op all@(x:y:z:xs) = throwError $ NumArgs 2 all
 numericBinop op params        = mapM unpackNum params >>= return . Number . foldl1 op
 
 
