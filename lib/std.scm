a2fbcfd7daa409ac13ae9ee9d816901ee97a477c
Reeze Xia <reeze@php.net>
2015-01-15 23:59:13 +0800

    Add std lib, it is so elegant... lisp is amazing

---
+++ lib/std.scm
@@ -0,0 +1,7 @@
+(define (not x) (if x #f #t))
+(define (null? obj) (if (eqv? obj '()) #t #f))
+(define (list . objs) objs)
+(define (id obj) obj)
+(define (flip func) (lambda (arg1 arg2) (func arg2 arg1)))
+(define (curry func arg1) (lambda (arg) (apply func (cons arg1 (list arg)))))
+(define (compose f g) (lambda (arg) (f (apply g arg))))
